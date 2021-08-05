8f5b92b65b28e7d9b31a522f622aef1a6c5e8ec1
Flam3rboy <34555296+Flam3rboy@users.noreply.github.com>
2021-08-05 23:25:48 +0200

    :bug: convert dynamic import to fixed

--- dist/structures/CategoryChannel.js
+++ dist/structures/CategoryChannel.js
@@ -7,6 +7,16 @@ const GuildChannel = require('./GuildChannel');
  */
 class CategoryChannel extends GuildChannel {
     /**
+     * @param {Guild} guild The guild the guild channel is part of
+     * @param {APIChannel} data The data for the guild channel
+     * @param {Client} [client] A safety parameter for the client that instantiated this
+     * @param {boolean} [immediatePatch=true] Control variable for patching
+     */
+    constructor(guild, data, client, immediatePatch = true) {
+        super(guild, data, client, immediatePatch);
+        this.collapsed = false;
+    }
+    /**
      * Channels that are a part of this category
      * @type {Collection<Snowflake, GuildChannel>}
      * @readonly
